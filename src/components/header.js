25cf9a22ec6df797282ea88be1eaff85c0837381
Andrei Geacar <andrei.geacar@gmail.com>
2016-11-09 16:30:15 +0200

    adding login functionality

---
+++ src/components/header.js
@@ -0,0 +1,50 @@
+'use strict';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+  ActivityIndicator
+} from 'react-native';
+import React, { Component } from 'react';
+
+export default class header extends Component {
+
+  render(){
+    return (
+      <View style={styles.header}>
+        <View style={styles.header_item}>
+          <Text style={styles.header_text}>{this.props.text}</Text>
+        </View>
+        <View style={styles.header_item}>
+        {  !this.props.loaded &&
+          <ActivityIndicator/>
+        }
+        </View>
+      </View>
+    );
+  }
+
+
+}
+
+const styles = StyleSheet.create({
+  header: {
+    padding: 10,
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginBottom: 20,
+    flex: 1
+  },
+  header_item: {
+    paddingLeft: 10,
+    paddingRight: 10
+  },
+  header_text: {
+    color: '#000',
+    fontSize: 18
+  }
+});
+
+AppRegistry.registerComponent('header', () => header);
